ddb61c356deb2694cf844ff8f2b5128b091be745
Shahad hatem <skbaalkhashir@stu.kau.edu.sa>
2024-05-03 15:03:07 +0300

    Your commit message

--- main.py
+++ main.py
@@ -114,10 +114,6 @@ async def trigger_eeg_processor():
 
 if __name__ == "__main__":
     import uvicorn
-<<<<<<< HEAD
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-=======
     import os
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
->>>>>>> 6ba11be6792a5982cf6520212b962996947154c8
